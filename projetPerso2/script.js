56c7f7d65850199b3a0edc6c388e2a1cd1601679
Rcohl <corentin.orczyk@outlook.fr>
2023-08-09 13:44:01 +0200

    portfolio v1

---
+++ projetPerso2/script.js
@@ -0,0 +1,125 @@
+const q1 = document.querySelector(".q1");
+// console.log(q1);
+
+const btn1 = document.querySelector(".btn1");
+// console.log(btn1);
+
+const btn2 = document.querySelector(".btn2");
+// console.log(btn2);
+
+const btn3 = document.querySelector(".btn3");
+// console.log(btn3);
+
+const btn4 = document.querySelector(".btn4");
+// console.log(btn4);
+
+const reponse = document.querySelector(".r1");
+// console.log(reponse);
+
+const q2 = document.querySelector(".q2");
+console.log(q2);
+
+const btn5 = document.querySelector(".btn5");
+console.log(btn5);
+
+const btn6 = document.querySelector(".btn6");
+console.log(btn6);
+
+const btn7 = document.querySelector(".btn7");
+console.log(btn7);
+
+const btn8 = document.querySelector(".btn8");
+console.log(btn8);
+
+const reponse2 = document.querySelector(".r2");
+console.log(reponse2);
+
+const q3 = document.querySelector(".q3");
+console.log(q3);
+
+const btn9 = document.querySelector(".btn9");
+console.log(btn9);
+
+const btn10 = document.querySelector(".btn10");
+console.log(btn10);
+
+const btn11 = document.querySelector(".btn11");
+console.log(btn11);
+
+const btn12 = document.querySelector(".btn12");
+console.log(btn12);
+
+const reponse3 = document.querySelector(".r3");
+console.log(reponse3);
+
+q1.addEventListener("click", () => {
+  q1.classList.add(".q1");
+});
+
+btn1.addEventListener("click", () => {
+  reponse.classList.add("show-reponse");
+  reponse.style.color = "red";
+});
+
+btn2.addEventListener("click", () => {
+  reponse.classList.add("show-reponse");
+  reponse.style.color = "red";
+});
+
+btn3.addEventListener("click", () => {
+  reponse.classList.add("show-reponse");
+  reponse.style.color = "green";
+});
+
+btn4.addEventListener("click", () => {
+  reponse.classList.add("show-reponse");
+  reponse.style.color = "red";
+});
+
+q2.addEventListener("click", () => {
+  q2.classList.add(".q2");
+});
+
+btn5.addEventListener("click", () => {
+  reponse2.classList.add("show-reponse");
+  reponse2.style.color = "red";
+});
+
+btn6.addEventListener("click", () => {
+  reponse2.classList.add("show-reponse");
+  reponse2.style.color = "red";
+});
+
+btn7.addEventListener("click", () => {
+  reponse2.classList.add("show-reponse");
+  reponse2.style.color = "green";
+});
+
+btn8.addEventListener("click", () => {
+  reponse2.classList.add("show-reponse");
+  reponse2.style.color = "red";
+});
+
+q3.addEventListener("click", () => {
+  q3.classList.add(".q3");
+});
+
+btn9.addEventListener("click", () => {
+  reponse3.classList.add("show-reponse");
+  reponse3.style.color = "red";
+});
+
+btn10.addEventListener("click", () => {
+  reponse3.classList.add("show-reponse");
+  reponse3.style.color = "red";
+});
+
+btn11.addEventListener("click", () => {
+  reponse3.classList.add("show-reponse");
+  reponse3.style.color = "red";
+});
+
+btn12.addEventListener("click", () => {
+  reponse3.classList.add("show-reponse");
+  reponse3.style.color = "green";
+});
