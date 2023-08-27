8961425745d96dd78616d9b2fef7e234d03ced46
Rcohl <corentin.orczyk@outlook.fr>
2023-08-27 11:10:40 +0200

    modifications du code html et css

---
+++ scriptProject.js
@@ -0,0 +1,16 @@
+// const imgGoblin = document.getElementById("goblin-project");
+// const imgProject1 = document.getElementById("personal-project1");
+// const imgProject2 = document.getElementById("personal-project2");
+// const imgBox = document.getElementsByClassName("box");
+
+// imgBox.addEventListener("mouseover", () => {
+//   imgGoblin.classList.add("show-goblin-project");
+// });
+
+// imgBox.addEventListener("mouseover", () => {
+//   imgProject1.classList.add("show-personal-project1");
+// });
+
+// imgBox.addEventListener("mouseover", () => {
+//   imgProject2.classList.add("show-personal-project2");
+// });
